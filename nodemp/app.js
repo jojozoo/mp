351e3bf5ae9786234e8ba6cee05a5599a7d0d51a
bp <515856563@qq.com>
2013-12-26 15:12:56 +0800

    m

--- nodemp/app.js
+++ nodemp/app.js
@@ -10,19 +10,27 @@ var express      = require('express')
 var app = express();
 
 // all environments
-app.set('port', process.env.PORT || 80);
+app.set('port', process.env.PORT || 8080);
 app.set('views', __dirname + '/views');
 app.set('view engine', 'ejs');
 // app.use(express.favicon('public/favicon.ico'));
 app.use(express.logger('dev'));
 // app.use(express.bodyParser());
-app.use(express.json());
-app.use(express.urlencoded());
+
+// // 等同于:
+// app.use(express.json());
+// app.use(express.urlencoded());
+// app.use(express.multipart());
+app.use(express.bodyParser());
 // 解析cookie
 app.use(express.cookieParser());
 // session存储和key
 
-app.use(express.bodyParser());
+app.use(express.session({
+    key: '_mp_session',
+    secret: '1c4e7ba050e27cfb027e68980fd5ba1299f19175b1e4c10bc2d934e811ae5a7c8e0a4716753aa25485948b62a8792fa920f423e459e1d5309f48a1ec4988512c',
+    cookie: {domain: '.manpai.com', path: '/'}
+}));
 
 app.use(express.static(path.join(__dirname, 'public')));
 
