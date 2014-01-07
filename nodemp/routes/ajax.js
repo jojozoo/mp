1b1c12aa6b90960c3e8d7c1ed955a5293be5fbc6
zhuxiaowu <zhuxiaowu@tianji.com>
2014-01-07 09:57:49 +0800

    rm

--- nodemp/routes/ajax.js
+++
@@ -1,8 +0,0 @@
-module.exports = function(app){
-
-    app.get('/check/:name', function(req, res){
-        console.log(req.query.key);
-        console.log(req.params.name);
-        res.send('success');
-    });
-}
