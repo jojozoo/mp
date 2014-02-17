2f2a68e7ce46b97d75d072c67a0021e82a4067fc
bp <515856563@qq.com>
2014-02-17 23:16:50 +0800

    fix bug

--- app/models/sbg.rb
+++ app/models/sbg.rb
@@ -2,8 +2,8 @@ class Sbg < ActiveRecord::Base
 	attr_accessible :photo, :bg, :del
 	# 为两种背景 一种登录背景和sign背景
     has_attached_file :photo
-    # scope :bgs -> {where(bg: true)}
-    # scope :signs -> {where(bg: false)}
+    scope :bgs, -> {where(bg: true)}
+    scope :signs, -> {where(bg: false)}
 
     validates_presence_of :photo, 
                         message: '不能为空'
