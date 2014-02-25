911c39530674abba6a6f6a6e07c9efd706fd12a3
zhuxiaowu <zhuxiaowu@tianji.com>
2014-02-25 18:32:27 +0800

    f

--- app/models/follow.rb
+++ app/models/follow.rb
@@ -1,7 +1,7 @@
 class Follow < ActiveRecord::Base
   attr_accessible :del, :follower_id, :mark, :user_id
   # follower_id 关注者
-  belongs_to :user
+  belongs_to :user, counter_cache: true
   belongs_to :follower, class_name: 'User'
 
   validates_uniqueness_of :user_id, 
