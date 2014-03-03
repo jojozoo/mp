b3df775a8b52e391dfe859b3efddf84e7508fd34
zhuxiaowu <zhuxiaowu@tianji.com>
2014-03-03 17:27:40 +0800

    f

--- app/controllers/works_controller.rb
+++ app/controllers/works_controller.rb
@@ -4,7 +4,7 @@ class WorksController < ApplicationController
     end
 
     def new
-        redirect_to works_path, notice: '请先登录' and return unless sign_in?
+        redirect_to images_path, notice: '请先登录' and return unless sign_in?
         # TODO 应该加上event_id 或者 album_id 索引条件
         @images = current_user.images.where(state: false, del: false)
         unless @event  = Event.ongoing.find_by_id(params[:eid])
