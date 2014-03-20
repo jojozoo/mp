37ec6b68c90417be91fb0dc2e3c03975de87cc35
zhuxiaowu <zhuxiaowu@tianji.com>
2014-03-20 15:58:57 +0800

    f

--- app/controllers/admin/images_controller.rb
+++ app/controllers/admin/images_controller.rb
@@ -1,2 +1,15 @@
 class Admin::ImagesController < Admin::ApplicationController
+	def index
+		# Date.today.beginning_of_day
+		# Date.today.end_of_day
+		date   = params[:date]  || Date.today.to_s
+		s_time = params[:stime] || '00:00:00'
+		e_time = params[:etime] || '59:59:59'
+		
+		@images = Image.between(date + ' ' + s_time, date + ' ' + e_time)
+	end
+
+	def tasks
+		
+	end
 end
