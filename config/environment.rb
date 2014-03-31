ca541670e25848a676fbcb29fadf8c0b3443e736
bp <515856563@qq.com>
2014-03-31 23:10:46 +0800

    f

--- config/environment.rb
+++ config/environment.rb
@@ -9,3 +9,10 @@ $site_config = {
     keywords: '漫拍,漫拍网,摄影活动,深度摄影,摄影名家,摄影展览,每天看时间最好的照片,快拍之星,摄影基地,摄影微博,摄影器材,摄影论坛,名家访谈,抢书读,市民摄影节 - ',
     description: '漫拍网，是一个当代图片社区网站，隔三差五发起一些又好玩又温暖，又有点意思又有点意义的各种漫拍活动。 - '
 }
+ActionMailer::Base.smtp_settings = {
+    :address => 'smtp.exmail.qq.com',
+    :port => 25,
+    :authentication => :login,
+    :user_name => 'service@shanpro.com',
+    :password => 'mpwang123'
+}
