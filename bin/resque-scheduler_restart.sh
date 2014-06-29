6763b0f2c1f84c382c5a68a5c20253c73fc4900a
zhuxaiowu <1@mpwang.com.cn>
2014-06-29 11:25:22 +0800

    f

--- bin/resque-scheduler_restart.sh
+++
@@ -1,2 +0,0 @@
-kill -9 `cat tmp/pids/resque-scheduler.pid`
-rake environment resque:scheduler PIDFILE='tmp/pids/resque-scheduler.pid' BACKGROUND=yes
--- bin/resque_restart.sh
+++ bin/resque_restart.sh
@@ -1,3 +1,5 @@
 #! /bin/sh
 kill -9 `cat tmp/pids/resque.pid`
-rake resque:work QUEUE='*' PIDFILE='tmp/pids/resque.pid' BACKGROUND=yes
+nohup rake resque:work QUEUE='*' PIDFILE='tmp/pids/resque.pid' VERBOSE=yes &
+kill -9 `cat tmp/pids/resque-scheduler.pid`
+rake environment resque:scheduler PIDFILE='tmp/pids/resque-scheduler.pid' BACKGROUND=yes  VERBOSE=yes
