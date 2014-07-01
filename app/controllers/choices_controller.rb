4774bb26fa764e03354fe0e25b3a194c90095690
zhuxaiowu <1@mpwang.com.cn>
2014-07-01 16:04:14 +0800

    f

--- app/controllers/choices_controller.rb
+++ app/controllers/choices_controller.rb
@@ -7,6 +7,6 @@ class ChoicesController < ApplicationController
 		@today = params[:id].to_date rescue Date.today
 		s_dt = (@today.to_s(:number) + "000000").to_time
 		e_dt = (@today.to_s(:number) + "235959").to_time
-		@choices = Photo.where(["recommend = ? and recommend_at > ? and recommend_at < ?",  true, s_dt, e_dt]).paginate(:page => params[:page], per_page: 12).order('id desc')
+		@choices = Photo.where(["recommend = ? and recommend_at > ? and recommend_at < ?",  true, s_dt, e_dt]).order('id desc')
 	end
 end
